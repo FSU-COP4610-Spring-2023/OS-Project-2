9caffb4b7c8afabd23b3ef3ee39755b054ac6aa6
StellaCG <67925940+StellaCG@users.noreply.github.com>
2023-03-05 16:26:58 -0500

    implemented bar initialization
    implemented initialization of the empty bar

--- Part3/bar.c
+++ Part3/bar.c
@@ -4,11 +4,15 @@
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
 #include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/slab.h>
 
+
 char* msg;
-int len, temp, accesses;
+int len, temp, accesses, i, j;
 struct timespec64 currentTime;
+static const struct proc_ops po;
+struct bar open_bar;
 
 struct customer{
 	struct timespec64 time_entered;
@@ -22,13 +26,13 @@ struct seat{
 };
 
 struct table{
-	struct seat mytable[8];
+	struct seat myseats[8];
 	int emptyseats;
 };
 
 struct bar{
 	struct customer queue[32];
-	struct table mybar[4];
+	struct table mytables[4];
 };
 
 MODULE_LICENSE("Dual BSD/GPL");
@@ -62,6 +66,15 @@ void create_new_proc_entry(void){
 static int bar_init(void){
 	printk(KERN_ALERT "Starting");
 	accesses=0;
+	// init bar
+	open_bar.full = false;
+	for (i = 0; i < 4; i++){
+	        for (j = 0; j < 8; j++){
+		                open_bar.mytables[i].myseats[j].empty = true;
+		                open_bar.mytables[i].myseats[j].clean = true;
+	        }
+	}
+	printk(KERN_ALERT "Bar opened");
 	create_new_proc_entry();
 	return 0;
 }
