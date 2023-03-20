636827e75b873a34264f0807bbabe7d40ae0a1b0
PaoloT1 <111890978+PaoloT1@users.noreply.github.com>
2023-03-19 23:39:33 -0400

    Added some kthread functionality and mutex locks
    NOTE: Code not tested so errors might be present

--- Part3/bar.c
+++ Part3/bar.c
@@ -8,6 +8,7 @@
 #include <linux/list.h>
 #include <linux/slab.h>
 #include <linux/delay.h>
+#include <linux/mutex.h>
 
 
 char* msg;
@@ -50,24 +51,31 @@ struct bar{
 struct waiter{
 	int currentTableNum;
 	struct table currentTable;
+	struct task_struct *kthread; // kthread declaration
+        struct mutex mutex; // lock declaration
 };
 
 // Waiter functions START
 
+int waiter_thread(void *data);
+
 void waiterInit(void){
     barWaiter.currentTableNum = 0;
     barWaiter.currentTable = open_bar.myTables[0];
-    waiter();
+    mutex_init(&barWaiter.mutex);
+    barWaiter.kthread = kthread_run(waiter_thread, NULL, "waiter");
 }
 
 void waiterMoveToNext(void){ // Sets up circular scan motion
     open_bar.status = MOVING;
-    mdelay(2000); // 2 second delay
+    //mdelay(2000); // use kernal timer instead
     barWaiter.currentTableNum = (barWaiter.currentTableNum + 1) % 4;
     barWaiter.currentTable = open_bar.myTables[barWaiter.currentTableNum];
 }
 
 void waiterClean(void){ // Cleans current table
+    // locking might need to be added
+    open_bar.status = CLEANING;
     struct timespec64 initialTime;
     int i;
     ktime_get_real_ts64(&initialTime);
@@ -81,6 +89,8 @@ void waiterClean(void){ // Cleans current table
 }
 
 void waiterRemove(void){ // Removes finished customers from current table
+    // free data allocated to each custoemr add check if seat is empty before removing customer
+    // locking also needs to be added
     int i;
     long long int timeElapsed;
     struct customer currentCustomer;
@@ -125,44 +135,54 @@ void waiterRemove(void){ // Removes finished customers from current table
 }
 
 void waiterAdd(void){
-    // error checking if queue is empty
-    
-    if (list_empty(&open_bar.queue) == 1) // if waiting queue is not empty
-    {
-        int i, groupSize;
-        struct list_head *firstList;
-        struct customer *currentCustomer;
-        firstList = list_first_entry(&open_bar.queue, struct list_head, list); // Grabs first group
-        currentCustomer = list_first_entry(firstList, struct customer, list); // Grabs first customer in group
-        groupSize = currentCustomer->group_id;
+    mutex_lock(&barWaiter.mutex); // only one thread can access at a time
 
-        for(i = 0; i < groupSize; i++)
-        {
-            if (barWaiter.currentTable.mySeats[i].empty == 1 && barWaiter.currentTable.mySeats[i].clean == 1) // if current seat is empty and clean
-            {
-                msleep(1000); // 1 second delay
-                barWaiter.currentTable.mySeats[i].empty = 0; // seat no longer empty
-                barWaiter.currentTable.mySeats[i].clean = 0; // seat no longer clean
-                barWaiter.currentTable.mySeats[i].current_customer = *currentCustomer; // sets new customer to seat
-                barWaiter.currentTable.emptySeats--; // removes empty seat from table
-                barWaiter.currentTable.cleanSeats--; // removes clean seat from table
-                ktime_get_real_ts64(&currentTime); // gets current time
-                currentCustomer->time_entered = currentTime; // updates time_entered with that time
-                currentCustomer = list_next_entry(currentCustomer, list); // moves on to next customer in group
-            }
-        }
-
-	// Deletes all elements in first spot of waiting queue
-	struct list_head *pos, *q;
-	list_for_each_safe(pos, q, &firstList->list){
-	    struct customer *entry = list_entry(pos, struct customer, list);
-	    if (entry == NULL)
-		break;
-	    list_del(pos);
-	    kfree(entry);
-	}
+    if (list_empty(&open_bar.queue)) { // if waiting queue empty, remove lock and return
+        mutex_unlock(&barWaiter.mutex);
+        return;
     }
+  
+    int i, groupSize;
+    struct list_head *firstList;
+    struct customer *currentCustomer;
+    firstList = list_first_entry(&open_bar.queue, struct list_head, list); // Grabs first group
+    currentCustomer = list_first_entry(firstList, struct customer, list); // Grabs first customer in group
+    groupSize = currentCustomer->group_id;
+
+    if (barWaiter.currentTable.cleanSeats < groupSize){ // if not enough clean seats for group, free lock and return
+        mutex_unlock(&barWaiter.mutex);
+	// free data 
+        return;
+    }
+
+    for(i = 0; i < groupSize; i++)
+    {
+        if (barWaiter.currentTable.mySeats[i].empty == 1 && barWaiter.currentTable.mySeats[i].clean == 1) // if current seat is empty and clean
+        {
+            msleep(1000); // 1 second delay
+            barWaiter.currentTable.mySeats[i].empty = 0; // seat no longer empty
+            barWaiter.currentTable.mySeats[i].clean = 0; // seat no longer clean
+            barWaiter.currentTable.mySeats[i].current_customer = *currentCustomer; // sets new customer to seat
+            barWaiter.currentTable.emptySeats--; // removes empty seat from table
+            barWaiter.currentTable.cleanSeats--; // removes clean seat from table
+            ktime_get_real_ts64(&currentTime); // gets current time
+            currentCustomer->time_entered = currentTime; // updates time_entered with that time
+            currentCustomer = list_next_entry(currentCustomer, list);
+        }
+    }
+
+    struct list_head *pos, *q;
+    list_for_each_safe(pos, q, &firstList->list){
+    	struct customer *entry = list_entry(pos, struct customer, list);
+	if (entry == NULL)
+	    break;
+	list_del(pos);
+	kfree(entry);
+    }
+
+    mutex_lock(&barWaiter.mutex); // free lock
 }
+
 
 // Waiter functions END
 
@@ -194,19 +214,24 @@ void create_new_proc_entry(void){
     temp=len;
 }
 
-void waiter(void){
-    while(open_bar.status == OFFLINE){
+// function that controls waiter kthread 
+int waiter_thread(void *data)
+{
+    while (!kthread_should_stop()) {
+	if (strcmp(open_bar.status, "IDLE") == 0){
+	    waiterRemove(); // 1. Remove customers who are done drinking at current table.
+
+            waiterAdd(); // 2. Add customers if possible (if clean and empty space).
+
+            waiterClean(); // 3. Clean table if possible (if table empty).
+
+            waiterMoveToNext(); // Move to next table
+	}
     }
-    while(open_bar.status != OFFLINE){
-        waiterRemove(); // 1. Remove customers who are done drinking at current table.
 
-        waiterAdd(); // 2. Add customers if possible (if clean and empty space).
-
-        waiterClean(); // 3. Clean table if possible (if table empty).
-
-        waiterMoveToNext(); // Move to next table
-    }
+    return 0;
 }
+
 
 static int bar_init(void){
     printk(KERN_ALERT "Starting");
@@ -227,7 +252,6 @@ static int bar_init(void){
 int customer_arrival(int number_of_customers, int type){
     int i;
     struct list_head *newList;
-    //struct customer *newCustomer;
 
     if (number_of_customers < 1 || number_of_customers > 8){ // number of customers in group not valid
         return 1;
@@ -251,6 +275,8 @@ int customer_arrival(int number_of_customers, int type){
 
 
 static void bar_exit(void){
+    kthread_stop(barWaiter.kthread); // stops the kthread
+    mutex_destroy(&barWaiter.mutex); // destroy mutex lock
     printk(KERN_ALERT "Exiting");
     kfree(msg);
     remove_proc_entry("majorsbar", NULL);
